afc757ce6600c48b1a9722050d65490189d2fefe
badrian123 <148589223+badrian123@users.noreply.github.com>
2026-08-26 19:10:50 -0700

    Add: top-k-elements-in-list - submission-27

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-27.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #I need to create a way to count and store my results
+        count = {}
+
+        #Then I need a way to organize my results in an order to access most frequent elements
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for key, value in count.items():
+            freq[value].append(key)
+
+        res = []
+
+        for p in range(len(freq)-1, 0,-1):
+            for v in freq[p]:
+                res.append(v)
+                if len(res) == k:
+                    return res
+        return res
